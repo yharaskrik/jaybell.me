437562376bc2f6d96b2a2ab62677320ca9d116ba
jaybell <jaycooperbell@gmail.com>
2024-04-21 14:43:51 -0700

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import {provideCloudflareLoader} from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
   providers: [
