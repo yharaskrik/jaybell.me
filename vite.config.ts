fcac71817f5f706d21d5febbb24fc2a18f3f18bb
jaybell <jaycooperbell@gmail.com>
2024-04-21 14:24:17 -0700

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.20.1
      project name = jaybellme
      framework = analog
      framework cli = create-analog@1.2.0
      package manager = npm@9.8.1
      wrangler = wrangler@3.51.2
      git = 2.44.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 
+import { Nitro } from 'nitropack';
+
+const devBindingsModule = async (nitro: Nitro) => {
+  if (nitro.options.dev) {
+    nitro.options.plugins.push('./src/dev-bindings.ts');
+  }
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   publicDir: 'src/assets',
@@ -12,7 +20,12 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [analog({
+    nitro: {
+      preset: "cloudflare-pages",
+      modules: [devBindingsModule]
+    }
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
